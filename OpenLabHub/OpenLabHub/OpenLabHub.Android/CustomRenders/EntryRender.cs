ea7834833251caeefd4a656df4c6f851d662bcd5
Matias <matiascasale1@gmail.com>
2023-10-08 08:30:12 -0300

    Api level changed to 13

---
+++ OpenLabHub/OpenLabHub/OpenLabHub.Android/CustomRenders/EntryRender.cs
@@ -0,0 +1,27 @@
+﻿using Android.Content;
+using Android.Graphics.Drawables;
+using Xamarin.Forms.Platform.Android;
+using OpenLabHub.Droid.CustomRenders;
+using Xamarin.Forms;
+using OpenLabHub.CustomRenders;
+
+[assembly: ExportRenderer(typeof(CustomEntry), typeof(EntryRender))]
+
+namespace OpenLabHub.Droid.CustomRenders
+{
+    public class EntryRender : EntryRenderer
+    {
+        public EntryRender(Context context) : base(context)
+        {
+            AutoPackage = false;
+        }
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged(e);
+            if (Control != null)
+            {
+                Control.Background = new ColorDrawable(Android.Graphics.Color.Transparent);
+            }
+        }
+    }
+}
---
+++ OpenLabHub/OpenLabHub/OpenLabHub/CustomRenders/CustomEntry.cs
@@ -0,0 +1,6 @@
+﻿using Xamarin.Forms;
+
+namespace OpenLabHub.CustomRenders
+{
+    public class CustomEntry : Entry {}
+}
